5afc7aebc4f1e064bb75ab1ade5d80141b881f02
ZuVi-VGM <vitog.m98@gmail.com>
2024-03-03 16:37:19 +0100

    Fixed game creation component

--- app/components/homepage.js
+++ app/components/homepage.js
@@ -5,16 +5,30 @@ import { useState } from 'preact/hooks';
 const HomePage = ({ mediator }) => {
     const [location, navigate] = useLocation();
     const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+
+    const createGameAndNavigate = async (username, password) => {
+        try {
+            // Chiamata asincrona per creare un nuovo gioco
+            const token = await mediator.createNewGame(username, password);
+            if(token)
+                navigate(`/game/${encodeURIComponent(token)}`);
+            else
+                alert('Error initializing game!');
+        } catch (error) {
+            // Gestire eventuali errori qui
+            console.error("Si è verificato un errore durante la creazione del gioco:", error);
+        }
+    };
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
         // Verifica che la password sia corretta (ad esempio, controlla la lunghezza, la complessità, ecc.)
-        if (password.length >= 5 && password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/)) {
-            //console.log(mediator);
-            mediator.passed = true;
-            navigate(`/game/${encodeURIComponent(password)}`);
+        if (username.trim().match(/^[a-zA-Z0-9]+$/) && password.length >= 5 && password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/)) {
+            //FUNZIONE ASINCRONA CHE DEVE RESTITUIRE TRUE PRIMA DEL NAVIGATE
+            createGameAndNavigate(username, password);
         } else {
-            alert('La password deve contenere almeno 5 caratteri, un numero e un carattere speciale!');
+            alert('L\' username deve contenere solo lettere e/o numeri.\r\nLa password deve contenere almeno 5 caratteri, un numero e un carattere speciale!');
         }
     };
 
@@ -22,6 +36,8 @@ const HomePage = ({ mediator }) => {
         <div>
             <h2>Crea una nuova partita</h2>
             <form onSubmit=${handleFormSubmit}>
+                <label for="username">Inserisci un username</label>
+                <input placeholder="Username" type="text" id="username" value=${username} onInput=${(e) => setUsername(e.target.value)} required />
                 <label for="password">Inserisci una password per accedere</label>
                 <input placeholder="Password" type="password" id="password" value=${password} onInput=${(e) => setPassword(e.target.value)} required />
                 <button type="submit">Invia</button>
