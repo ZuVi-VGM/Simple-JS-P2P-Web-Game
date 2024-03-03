42bcaf3d9846e4046b44f0cafc89ecf6f900512b
ZuVi-VGM <vitog.m98@gmail.com>
2024-03-03 16:38:37 +0100

    Created utility functions to start or connect to a game

--- app/mediator/mediator.js
+++ app/mediator/mediator.js
@@ -1,8 +1,16 @@
+import PeerManager from '../network/peerManager.js';
+import MessageHandler from '../network/messageHandler.js';
+import EncryptionService from "../services/encryptionService.js";
+import AuthenticationService from "../services/authService.js";
+import Game from '../models/game.js';
+
 class Mediator {
-    constructor(PeerManager, MessageHandler, Game) {
-        this.peer = PeerManager;
-        this.messageHandler = MessageHandler;
-        this.game = Game;
+    constructor() {
+        this.peer = new PeerManager;
+        this.messageHandler = new MessageHandler;
+        this.game = new Game;
+        this.encryption = new EncryptionService;
+        this.auth = new AuthenticationService;
 
         this.peer.emitter.on('dataReceived', (data) => {
             this.messageHandler.handleMessage(data);
@@ -32,6 +40,59 @@ class Mediator {
             Object.values(this.game.users).forEach(user => {
                 this.peer.closeConnection(user.connection);
             });
+        }
+    }
+
+    async createNewGame(username, password){
+        //Use password to initialize the game:
+        //create salt and key for hmac - DONE
+        //save myid - DONE
+        //create RSA keys - DONE
+        //create token - DONE
+        try
+        {
+            await this.encryption.generateHmacKey(password);
+            //create token
+            const tokenObj = await this.auth.post('token/create', {'peer_id': this.peer.peer_id, 'salt': this.encryption.hmacObj['salt']});
+
+            //console.log(tokenObj['data']['token']);
+            //console.log(this.game.users);
+
+            this.game.isHost = true;
+            if(!this.game.addHost(this.peer.peer_id, username))
+                throw new Error('Cannot add host, check for correct username!');
+
+            await this.encryption.init('rsa');
+            if(!this.game.setPubKey(this.encryption.keyPair.publicKey))
+                throw new Error('Cannot add host, problem with RSA Key!');
+
+            return tokenObj['data']['token'];
+        } catch(error) {
+            console.error("Error initializing game:", error);
+            this.game.isHost = false;
+            return false;
+        }
+    }
+
+    async initConnection(token){
+        //Get Peer_Id from Token
+        //Connect and init connection
+        try{
+            //Validate Token
+            const tokenObj = await this.auth.get('token/validate/' + token);
+            if(tokenObj['data']['error'])
+                throw new Error('Invalid Token!');
+
+            //Try to connect to host
+            if(!this.peer.connectToPeer(tokenObj['data']['payload']['peer_id']))
+                throw new Error('Error during connection!');
+            
+            this.game.updateStatus();
+            return true;
+        } catch(error) {
+            console.error("Error initializing game:", error);
+            this.game.isHost = false;
+            return false;
         }
     }
 }
