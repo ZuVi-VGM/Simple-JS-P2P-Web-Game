8cb882b63671bd22420bd02fd048bff87c2feabe
ZuVi-VGM <vitog.m98@gmail.com>
2024-03-03 16:38:05 +0100

    80% done with game component (game connection/start/play ecc)

--- app/components/game.js
+++ app/components/game.js
@@ -6,20 +6,53 @@ const Game = ({ mediator }) => {
     const [loading, setLoading] = useState(true);
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    const [authenticated, setAuthenticated] = useState(false);
+    const [authenticated, setAuthenticated] = useState(mediator.game.isHost);
     const [showLoginForm, setShowLoginForm] = useState(true);
+    const [gameStarted, setGameStarted] = useState(false);
     const [messages, setMessages] = useState([
         { text: 'Messaggio Ricevuto' },
         { text: 'Messaggio Inviato', sender: true }
     ]);
+    
     const [newMessage, setNewMessage] = useState('');
     const messagesEndRef = useRef(null);
+
+    const [userList, setUsers] = useState(Object.values(mediator.game.users).map(user => user.name));
+
+    useEffect(() => {
+        // Aggiorna lo stato locale quando il valore di users in mediator cambia
+        const updateHandler = () => {
+            setUsers(Object.values(mediator.game.users).map(user => user.name));
+            console.log(Object.values(mediator.game.users).map(user => user.name));
+        };
+
+        // Aggiungi il componente come osservatore di Foo
+        mediator.game.addObserver({ update: updateHandler });
+
+        return () => {
+            // Rimuovi il componente come osservatore quando il componente viene smontato
+            mediator.game.removeObserver({ update: updateHandler });
+        };
+    }, [mediator.game]);
 
     // Ottieni il parametro token dall'URL
     const [location, navigate] = useLocation();
 
     const token = location.split('/game/')[1];
 
+    const initConnection = async (token) => {
+        try {
+            // Chiamata asincrona per creare un nuovo gioco
+            //await mediator.peer.getId();
+            if(await mediator.initConnection(token))
+                console.log(mediator);
+            else
+                return html`Error`;
+        } catch (error) {
+            // Gestire eventuali errori qui
+            console.error("Si è verificato un errore durante la connessione:", error);
+        }
+    };
 
     useEffect(() => {
         // Se il token è vuoto, reindirizza alla homepage
@@ -27,6 +60,16 @@ const Game = ({ mediator }) => {
             navigate('/');
         } else {
             // Altrimenti, esegui il resto del codice
+            // Qui faccio la prima parte di autenticazione se non sono host
+            if(!mediator.game.isHost){
+                //Authentication
+                //Get token info
+                //Init connection
+                //Init e2e
+                //Then authenticate
+                
+                initConnection(token);
+            } 
             // Simulazione del caricamento completato
             const timer = setTimeout(() => {
                 setLoading(false);
@@ -54,6 +97,13 @@ const Game = ({ mediator }) => {
         }
     }, [messages]);
 
+    useEffect(() => {
+        if(gameStarted){
+            mediator.game.started = true;
+            console.log(mediator);
+        }
+    }, [gameStarted]);
+
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
@@ -64,7 +114,7 @@ const Game = ({ mediator }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Simulazione di script che controlla la password
+        //Simulazione di script che controlla la password
         /* CHIAMA SCRIPT */
         if (password === 'password_corretta') {
             setShowLoginForm(false);
@@ -72,6 +122,7 @@ const Game = ({ mediator }) => {
             setError('Password errata! Riprova.');
             setPassword('');
         }
+        //setGameStarted(true);
     };
 
     const handleMessageSubmit = (e) => {
@@ -80,6 +131,12 @@ const Game = ({ mediator }) => {
             setMessages([...messages, { text: newMessage, sender: true }]);
             setNewMessage('');
         }
+    };  
+
+    const renderUserList = () => {
+        return userList.map(user => (
+            html`<li>${user}</li>`
+        ));
     };
 
     if (loading) {
@@ -87,7 +144,7 @@ const Game = ({ mediator }) => {
         return html`<p>Caricamento...</p>`;
     }
 
-    if (!authenticated) {
+    if (!authenticated && !mediator.game.isHost && !gameStarted) {
         return (
             html`<div class="login-form ${!showLoginForm ? 'hide' : ''}">
                 <h2>Inserisci la password:</h2>
@@ -103,24 +160,47 @@ const Game = ({ mediator }) => {
                 ${error &&  html`<p>${error}</p>`}
             </div>`
         );
+    } else {
+        setAuthenticated(true);
     }
 
-    // Se l'utente è autenticato, mostriamo la chatbox
+    // Se l'utente è autenticato, mostriamo la sala d'attesa
+    if(authenticated && !gameStarted){
+        return (
+            html`
+            <div>
+                <h2>Utenti Collegati:</h2>
+                <ul>
+                    ${renderUserList()}   
+                </ul>
+                <p>Attendi che il game inizi...</p>
+                ${mediator.game.isHost &&
+                    html`<button onClick=${() => setGameStarted(true)}>Inizia Gioco</button>`
+                }
+                <button onClick=${() => setUsers({...userList, 'test': {'name': 'prova'}})}>adduser</button>
+                <button onClick=${() => console.log(mediator)}>adduser</button>
+                ${error && html`<p>${error}</p>`}
+            </div>
+            `
+        );
+    }
+
+    // Se il game è iniziato mostriamo la chatbox
     return (html`
-    <div class="chatbox-container">
-    <div class="title">Parola da indovin___</div>
-    <div class="messages">
-        ${messages.map((message, index) => html`
-            <div class="message ${message.sender ? 'sender' : ''}" key=${index}>${message.text}</div>
-        `)}
-        <div ref=${messagesEndRef}></div>
-    </div>
-    <div class="input-box">
-        <form onSubmit=${handleMessageSubmit}>
-            <input type="text" placeholder="Digita il tuo messaggio..." value=${newMessage} onInput=${handleMessageChange} />
-            <button type="submit">Invia</button>
-        </form>
-    </div></div>`
+        <div class="chatbox-container">
+        <div class="title">Parola da indovin___</div>
+        <div class="messages">
+            ${messages.map((message, index) => html`
+                <div class="message ${message.sender ? 'sender' : ''}" key=${index}>${message.text}</div>
+            `)}
+            <div ref=${messagesEndRef}></div>
+        </div>
+        <div class="input-box">
+            <form onSubmit=${handleMessageSubmit}>
+                <input type="text" placeholder="Digita il tuo messaggio..." value=${newMessage} onInput=${handleMessageChange} />
+                <button type="submit">Invia</button>
+            </form>
+        </div></div>`
     );
 };
 
