08172b5387fac73942c520bca06b94c56a2bb8ba
ZuVi-VGM <vitog.m98@gmail.com>
2024-03-03 16:39:06 +0100

    Edited models and implemented observable class logic

--- app/models/game.js
+++ app/models/game.js
@@ -3,28 +3,63 @@ import User from "./user.js";
 class Game {
     constructor() {
         this.users = {};
+        this.obs = [];
         this.started = false;
+        this.isHost = false;
+        this.changed = 0;
     }
 
     startGame(){
         this.started = true;
+        this.notify();
+    }
+
+    addHost(peer_id, username)
+    {
+        if(this.started)
+            return false;
+
+        this.users['host'] = new User(peer_id);
+
+        if(!this.users['host'].setName(username))
+        {
+            this.deleteUser('host');
+            return false;
+        }
+
+        this.notify();
+        return true;
+    }
+
+    setPubKey(key)
+    {
+        if(!this.users['host'])
+            return false;
+
+        this.users['host']['pkey'] = key;
+        return true;
     }
 
     addUser(conn) {
-        if(this.started)
+        if(this.started && this.users.length >= 10)
             return false;
         
         this.users[conn.peer] = new User(conn);
+        //this.updateStatus();
+        this.notify();
         return true;
     }
 
     deleteUser(id) {
         delete this.users[id];
+        this.notify();
     }
 
     givePoint(id){
-        if(this.users[id])
+        if(this.users[id]){
             this.users[id].givePoint();
+            this.notify();
+        } 
     }
 
     getUserStats(){
@@ -35,6 +70,24 @@ class Game {
             });
         }
         return data;
+    }
+
+    updateStatus(){
+        this.changed = 1;
+        this.notify();
+    }
+
+    addObserver(obs) {
+        this.obs.push(obs);
+    }
+
+    removeObserver(obs) {
+        this.obs = this.obs.filter(obse => obse !== obs);
+    }
+
+    notify(){
+        console.log(this.obs);
+        this.obs.forEach(observer => observer.update())
     }
 }
 
--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,12 @@
 class User {
     constructor(connection){
         this.connection = connection;
-        this.name = '';
+        this.name = 'aaasfa';
         this.points = 0;
     }
 
     setName(name){
-        if(typeof name !== 'string' || name.trim().length === 0)
+        if(typeof name !== 'string' || name.trim().length === 0 || !name.trim().match(/^[a-zA-Z0-9]+$/))
             return false;
 
         this.name = name.trim();
