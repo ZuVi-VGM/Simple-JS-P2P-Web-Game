5a59ee77406df38aa5ddd4be95724df5b5c74d37
ZuVi-VGM <vitog.m98@gmail.com>
2024-03-08 20:59:31 +0100

    Added utility functions to handle game status

--- app/mediator/mediator.js
+++ app/mediator/mediator.js
@@ -14,12 +14,38 @@ class Mediator {
 
         this.peer.emitter.on('dataReceived', (data) => {
             this.messageHandler.handleMessage(data);
+
+            switch(data['action']){
+                case 'authenticate':
+                    this.setUserName(data);
+                    break;
+                case 'update_users':
+                    this.game.updateUsersList(data['payload']);
+                    break;
+                case 'delete_user':
+                    this.game.deleteUser(data['peer']);
+                    break;
+                case 'start_game':
+                    this.startGame(data);
+                    break;
+                case 'new_message':
+                    this.addMessage(data);
+                    break;
+                case 'give_point':
+                    this.givePoint(data);
+                    break;
+                case 'game_end':
+                    this.endGame(data);
+                    break;
+                // default: //maybe handle case where action is not defined?
+                //     this.messageHandler.handleMessage(data);
+            }
         });
 
         this.peer.emitter.on('newConnection', (conn) => {
             if(!this.game.addUser(conn)) {
                 setTimeout(() => {
-                    this.messageHandler.sendMessage(conn, 'Connection Refused, game started!');
+                    this.messageHandler.sendMessage(conn, 'Connection Refused, game started!'); //TODO: maybe remove this and handle game started
                     this.peer.closeConnection(conn);
                 }, 500);
             }
@@ -27,9 +53,28 @@ class Mediator {
 
         this.peer.emitter.on('connectionClosed', (conn) => {
             this.game.deleteUser(conn.peer);
+            this.notifyAll({'action': 'delete_user', 'peer': conn.peer});
         });
 
         window.addEventListener('beforeunload', (event) => this.handleBeforeUnload(event));
+    }
+
+    notifyAll(data){
+        if(!this.game.isHost)
+            return;
+
+        if (Object.keys(this.game.users).length > 0) {
+            Object.values(this.game.users).forEach(user => {
+                if(user.connection.peer)
+                    this.messageHandler.sendMessage(user.connection, data);
+            });
+        }
+    }
+
+    setUserName(data){
+        this.game.users[data['peer_id']].setName(data['value']);
+        this.notifyAll({'action': 'update_users', 'payload': this.game.getUserData()});
+        this.game.notify();
     }
 
     handleBeforeUnload(event){
@@ -42,6 +87,72 @@ class Mediator {
             });
         }
     }
+
+    sendMessage(message)
+    {
+        let data = {'action': 'new_message', 'text': message, 'peer_id': this.peer.peer_id};
+        if(!this.game.isHost)
+            this.messageHandler.sendMessage(this.game.users[this.game.host].connection, data);
+        else
+            this.addMessage(data);
+    }
+
+    addMessage(data){
+        this.game.message = {'text': data['text'], 'sender': false};
+
+        if(data['peer_id'] == this.peer.peer_id)
+            this.game.message['sender'] = true;
+        
+        this.notifyAll(data);
+        this.game.notify(); 
+
+        if(this.game.isHost)
+            this.checkForWin(data);       
+    }
+
+    checkForWin(data){
+        if(data['text'].toLowerCase() == this.game.curr_word['parola'].toLowerCase())
+            this.victory(data['peer_id']);
+    }
+
+    givePoint(data){
+        this.game.curr_word = data['word'];
+        this.game.givePoint(data['peer_id']);
+    }
+
+    startGame(data = null){
+        if(this.game.isHost){
+            this.game.curr_word = this.game.words.pop();
+            this.notifyAll({'action':'start_game', 'word': this.game.curr_word}); //TODO: send also a timestamp to sincronize
+        }
+        else
+            this.game.curr_word = data['word'];
+
+        this.game.startGame();           
+    }
+
+    victory(peer_id){
+        if(this.game.isHost){
+            if(this.game.words.length > 0){
+                this.game.curr_word = this.game.words.pop();
+                this.notifyAll({'action':'give_point', 'peer_id':peer_id, 'word': this.game.curr_word});
+                
+            } else {
+                this.notifyAll({'action':'game_end', 'peer_id': peer_id});
+                this.game.started = false; //Handle this case
+            }
+        }
+
+        if(!this.game.givePoint(peer_id))
+            this.game.givePoint('host');
+    }
+
+    endGame(peer_id){
+        this.game.givePoint(peer_id);
+        this.game.started = false;
+        notify();
+    }
+
 
     async createNewGame(username, password){
         //Use password to initialize the game:
@@ -65,7 +176,8 @@ class Mediator {
             await this.encryption.init('rsa');
             if(!this.game.setPubKey(this.encryption.keyPair.publicKey))
                 throw new Error('Cannot add host, problem with RSA Key!');
-
+            
+            this.game.createNewGame();
             return tokenObj['data']['token'];
         } catch(error) {
             console.error("Error initializing game:", error);
@@ -74,7 +186,31 @@ class Mediator {
         }
     }
 
-    async initConnection(token){
+    async initConnection(token, username){
+
+        //Create connection and authenticate
+            //Init connection
+            //Init e2e -> future implementation
+            //Then authenticate
+        
+        try{
+            const connectionSuccessful = await this.peer.connectToPeer(token['peer_id']);
+            if(!connectionSuccessful)
+                return false;
+
+            this.game.host = token['peer_id'];
+            let query = {'action': 'authenticate', 'value': username, 'peer_id': this.peer.peer_id};
+            
+            this.messageHandler.sendMessage(this.game.users[token['peer_id']].connection, query);
+            //this.game.updateStatus();
+            return true;  
+        } catch (err) {
+            console.error('Errore durante l\'inizializzazione della connessione:', err);
+            return false;
+        }
+    }
+
+    async validateToken(token){
         //Get Peer_Id from Token
         //Connect and init connection
         try{
@@ -83,15 +219,9 @@ class Mediator {
             if(tokenObj['data']['error'])
                 throw new Error('Invalid Token!');
 
-            //Try to connect to host
-            if(!this.peer.connectToPeer(tokenObj['data']['payload']['peer_id']))
-                throw new Error('Error during connection!');
-            
-            this.game.updateStatus();
-            return true;
+            return tokenObj['data']['payload'];
         } catch(error) {
-            console.error("Error initializing game:", error);
-            this.game.isHost = false;
+            console.error("Error validating token:", error);
             return false;
         }
     }
